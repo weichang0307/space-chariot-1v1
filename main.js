f3050188b0c71eb4783e73038ee7d7b7e1c2498f
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2023-02-04 13:21:14 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,202 @@
+let canvas=document.getElementById('canvas')
+ww=1600
+wh=800
+let ctx=canvas.getContext('2d')
+mysize()
+let fps=50
+let ws=[]
+let tt=0
+
+
+let car1
+let car2
+let barriers=[]
+let information
+
+
+
+
+
+let world_=new world(0,0,10)
+let keys={}
+function init(){
+
+
+
+    ws.push(new polygon(800,800,[[-800,-10],[800,-10],[800,10],[-800,10]],Infinity))
+    ws.push(new polygon(800,0,[[-800,-10],[800,-10],[800,10],[-800,10]],Infinity))
+    ws.push(new polygon(1600,500,[[-10,-500],[10,-500],[10,500],[-10,500]],Infinity))
+    ws.push(new polygon(0,500,[[-10,-500],[10,-500],[10,500],[-10,500]],Infinity))
+    for(let i of ws){
+        i.isgravity=false
+        world_.add(i)
+    }
+
+    for(let i=0;i<10;i++){
+        let r=Math.random()*100+50
+        let ps=[]
+        for(let rad=0;rad<Math.PI*2;rad+=Math.random()*1+1){
+            let p0=[r*Math.cos(rad),r*Math.sin(rad)]
+            ps.push(p0)
+        }
+        b0=new polygon(Math.random()*1400+100,Math.random()*600+100,ps,r**2/100)
+        b0.velocity=[Math.random()*40-20,Math.random()*40-20]
+        b0.omega[2]=Math.random()-0.5
+        b0.mass=b0.area*0.001
+        world_.add(b0)
+        barriers.push(b0)
+    }
+
+
+
+    car1=new Car(100,100)
+    world_.add(car1)
+    car1.velocity=[100,0]
+    car1.color='red'
+
+    car2=new Car(1500,700)
+    world_.add(car2)
+    car2.velocity=[-100,0]
+    car2.color='orange'
+    car2.control_keys={lf:'Numpad7',lb:'Numpad4',rf:'Numpad9',rb:'Numpad6',fire:'Numpad0'}
+
+    information=new Information(1550,50)
+ 
+
+
+
+    world_.setCoefficient('default','default',1,0.5)
+
+
+    window.addEventListener('keydown',keydown)
+    window.addEventListener('keyup',keyup)
+    document.addEventListener('click',onclick)
+
+    
+}
+
+
+function update(){
+
+    tt+=1000/fps
+
+
+    car1.update(keys)
+    car2.update(keys)
+    
+
+    world_.update(1/fps)
+
+}
+
+function draw(){
+    ctx.fillStyle='black'
+    ctx.fillRect(0,0,1600,1000)
+
+    car1.draw()
+    car2.draw()
+    
+
+    for(let i of ws){
+        world_.draw_helper(i,'yellow')
+    }
+    for(let i of barriers){
+        world_.draw_helper(i,'rgb(200,100,50)')
+    }
+    information.draw()
+
+    requestAnimationFrame(draw)
+}
+
+function keydown(e){
+    keys[e.code]=true
+    car1.keydown(e)
+    car2.keydown(e)
+
+}
+function keyup(e){
+    keys[e.code]=false
+    car1.keyup(e)
+    car2.keyup(e)
+}
+function onclick(e){
+    information.onclick(get_p_in_world(e.pageX,e.pageY))
+}
+
+
+function mysize(){
+	if(window.innerHeight/window.innerWidth>=wh/ww){
+		canvas.style.width=window.innerWidth+'px'
+		canvas.style.height=wh*window.innerWidth/ww+'px'
+		canvas.width=window.innerWidth
+		canvas.height=wh*window.innerWidth/ww
+		canvas.position='absolute'
+		canvas.left=window.innerWidth-canvas.width/2
+		canvas.top=0
+		canvas.style.position='absolute'
+		canvas.style.left=0+'px'
+		canvas.style.top=0+'px'
+	}else{
+		canvas.style.width=ww*window.innerHeight/wh+'px'
+		canvas.style.height=window.innerHeight+'px'
+		canvas.width=ww*window.innerHeight/wh
+		canvas.height=window.innerHeight
+		canvas.style.position='absolute'
+		canvas.style.left=(window.innerWidth-canvas.width)/2+'px'
+		canvas.style.top=0+'px'
+		
+	}
+	ctx.restore()
+
+	if(window.innerHeight/window.innerWidth>=wh/ww){
+		ctx.scale(window.innerWidth/ww,window.innerWidth/ww)
+	}else{
+		ctx.scale(window.innerHeight/wh,window.innerHeight/wh)
+	}
+	
+	
+}
+
+
+function get_p_in_world(x,y){
+	let fx=(x-parseFloat(canvas.style.left))*ww/canvas.width
+	let fy=(y-parseFloat(canvas.style.top))*ww/canvas.width
+	return [fx,fy]
+}
+
+init()
+setInterval(update,1000/fps)
+draw()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ objects.js
@@ -0,0 +1,298 @@
+class Car extends polygon{
+    constructor(x,y){
+        super(x,y,[[0,0],[70,0],[75,25],[70,50],[0,50],[-5,25]],10,'defult',0.3,0.7)
+        this.add_force('rf',[30,0],[0,0])
+        this.add_force('rb',[30,0],[0,0])
+        this.add_force('lf',[-30,0],[0,0])
+        this.add_force('lb',[-30,0],[0,0])
+        this.power={rf:0,rb:0,lf:0,lb:0}
+        this.bullets=[]
+        this.control_keys={rf:'KeyY',rb:'KeyG',lf:'KeyE',lb:'KeyD',fire:'KeyV'}
+        this.cooldown=0
+        this.hp=5
+        this.hpbar_transparent=2
+        this.color='red'
+        this.collision=(e)=>{
+            if(e.object.type==='ball'){
+                this.hp-=1
+                this.hpbar_transparent=2
+            }
+        }
+
+    }
+    fire(){
+        if(this.cooldown<=0){
+           this.cooldown=1
+            let p=myVector.add(this.position,myVector.rotate([0,-35],this.angle))
+            let bb=new Bullet(p[0],p[1],this)
+            bb.velocity=myVector.add(this.velocity,myVector.rotate([0,-3000],this.angle))
+            this.velocity=myVector.minus(this.velocity,myVector.rotate([0,-300],this.angle))
+            this.world.add(bb)
+            this.bullets.push(bb) 
+        }
+        
+    }
+    keydown(e){
+        if(e.code===this.control_keys.fire){
+            this.fire()
+        }
+        if(e.code==='Space'){
+            this.hpbar_transparent=2
+        }
+    }
+    keyup(e){
+        for(let i in this.control_keys){
+            if(e.code===this.control_keys[i]&&i!=='fire'){
+                this.get_force(i).force=[0,0]
+                this.power[i]=0
+                break
+            }
+            
+        }
+
+    }
+    update(keys){
+        this.cooldown-=1/50
+        if(this.hpbar_transparent>0&&this.hp>0){
+            this.hpbar_transparent-=0.01
+            if(this.hpbar_transparent<0){
+                this.hpbar_transparent=0
+            }
+        }
+        for(let i of this.bullets){
+            i.update()
+        }
+
+        if(keys[this.control_keys.lb]){
+            this.get_force('lb').force=myVector.rotate([0,-1000],this.angle)
+            this.power.lb+=1
+        }
+        if(keys[this.control_keys.lf]){
+            this.get_force('lf').force=myVector.rotate([0,1000],this.angle)
+            this.power.lf+=1
+        }
+        if(keys[this.control_keys.rf]){
+            this.get_force('rf').force=myVector.rotate([0,1000],this.angle)
+            this.power.rf+=1
+        }
+        if(keys[this.control_keys.rb]){
+            this.get_force('rb').force=myVector.rotate([0,-1000],this.angle)
+            this.power.rb+=1
+        }
+    }
+    draw(ctx_=ctx){
+        let color=this.color
+        //draw bullets
+        for(let i of this.bullets){
+            i.draw()
+        }
+
+        //draw car
+        ctx_.save()
+        ctx_.translate(this.position[0],this.position[1])
+        ctx_.rotate(this.angle)
+            //body
+            ctx_.fillStyle=color
+            ctx_.fillRect(-20,-25,40,50)
+            //left engine
+            ctx_.beginPath()
+            ctx_.moveTo(-25,25)
+            ctx_.lineTo(-20,0)
+            ctx_.lineTo(-25,-25)
+            ctx_.lineTo(-35,-25)
+            ctx_.lineTo(-40,0)
+            ctx_.lineTo(-35,25)
+            ctx_.closePath()
+            ctx_.fill()
+            //right engine
+            ctx_.beginPath()
+            ctx_.moveTo(25,25)
+            ctx_.lineTo(20,0)
+            ctx_.lineTo(25,-25)
+            ctx_.lineTo(35,-25)
+            ctx_.lineTo(40,0)
+            ctx_.lineTo(35,25)
+            ctx_.closePath()
+            ctx_.fill()
+            //canon
+            ctx_.beginPath()
+            ctx_.arc(0,5,15,0,Math.PI*2)
+            ctx_.closePath()
+            ctx_.fillStyle='rgb(100,100,100)'
+            ctx_.fill()
+            ctx_.fillStyle='rgb(100,100,100)'
+            ctx_.fillRect(5-10,0-40,10,40)
+            //power
+            let grd=ctx_.createLinearGradient(-5,0,5,0)
+            grd.addColorStop(0,'blue')
+            grd.addColorStop(0.5,'white')
+            grd.addColorStop(1,'blue')
+            ctx_.fillStyle=grd
+            for(let i in this.power){
+                if(this.power[i]!==0){
+                    let length=40*this.power[i]/5
+                    ctx_.save()
+
+                    let x_
+                    let y_
+                    let isupsidedown=false
+                    if(i==='lf'||i==='lb'){
+                        x_=-30
+                        if(i=='lf'){
+                            y_=-25
+                        }else{
+                            y_=25
+                            isupsidedown=true
+                        }
+                    }else{
+                        x_=30
+                        if(i==='rf'){
+                            y_=-25
+                        }else{
+                            y_=25
+                            isupsidedown=true
+                        }
+                    }
+                    
+                    ctx_.translate(x_,y_)
+                    if(isupsidedown){
+                        ctx_.rotate(Math.PI)
+                    }
+                    ctx_.beginPath()
+                    ctx_.moveTo(-5,0)
+                    ctx_.lineTo(5,0)
+                    ctx_.lineTo(0,-length)
+                    ctx_.closePath()
+                    ctx_.fill()
+                    ctx_.restore()
+                    this.power[i]%=5
+                }
+                
+            }
+
+            //draw hp bar
+            ctx_.globalAlpha=this.hpbar_transparent
+            ctx_.rotate(-this.angle)
+            ctx_.fillStyle='rgb(0,225,0)'
+            ctx_.fillRect(-40,-60,80*this.hp/5,10)
+            ctx_.strokeStyle='white'
+            ctx_.lineWidth =3
+            ctx_.strokeRect(-40,-60,80,10)
+            ctx_.globalAlpha=1
+
+            
+
+
+
+
+
+
+
+        ctx_.restore()
+        
+    }
+}
+class Bullet extends ball{
+    constructor(x,y,car){
+        super(x,y,5,1)
+        this.car=car
+        this.collision=(e)=>{
+            this.boom()
+        }
+        this.state=0
+    }
+    boom(){
+        this.world.delete(this)
+        this.state=1
+    }
+    update(){
+        if(this.state>0){
+            this.state+=(11-this.state)/10
+            if(this.state>10){
+                for(let i in this.car.bullets){
+                    if(this.car.bullets[i]===this){
+                        this.car.bullets.splice(i,1)
+                    }
+                }
+            }
+        }
+        
+    }
+    draw(ctx_=ctx){
+        ctx_.save()
+        ctx_.translate(this.position[0],this.position[1])
+            if(this.state===0){
+                ctx_.beginPath()
+                ctx_.arc(0,0,5,0,Math.PI*2)
+                ctx_.closePath()
+                ctx_.fillStyle='white'
+                ctx_.fill()
+            }else{
+                ctx_.globalAlpha=(10-this.state)/10
+                ctx_.fillStyle='orange'
+
+                ctx_.beginPath()
+                ctx_.arc(0,0,this.state*10,0,Math.PI*2)
+                ctx_.closePath()
+                ctx_.fill()
+                ctx_.beginPath()
+                ctx_.arc(0,0,this.state*6,0,Math.PI*2)
+                ctx_.closePath()
+                ctx_.fill()
+
+                ctx_.globalAlpha=1
+            }
+            
+
+
+        ctx_.restore()
+    }
+}
+class Information{
+    constructor(x,y){
+        this.position=[x,y]
+        this.state=0
+        this.text='Player1 \n控制引擎:E,D,Y,G  發射子彈:V \nPlayer2 \n控制引擎:7,4,9,6  發射子彈:0 \n \n一共有五滴血 被子彈打到扣一滴 \n按空白鍵可察看雙方血量'
+        this.textArray=this.text.split('\n')
+    }
+    onclick(click_point){
+        if(this.state===0){
+            if(click_point[0]>1500&&click_point[1]<100){
+                this.state=1
+            }
+        }else if(this.state===1){
+            this.state=0
+        }
+    }
+    draw(ctx_=ctx){
+        ctx_.globalAlpha=0.5
+        if(this.state===0){
+            ctx_.beginPath()
+            ctx_.arc(this.position[0],this.position[1],25,0,Math.PI*2)
+            ctx_.closePath()
+            ctx_.strokeStyle='white'
+            ctx_.lineWidth=5
+            ctx_.stroke()
+
+            ctx_.font='45px serif'
+            ctx_.fillStyle='white'
+            ctx_.fillText('i',1543,63)
+        }else if(this.state===1){
+            ctx_.fillStyle='black'
+            ctx_.fillRect(0,0,1600,800)
+            ctx_.globalAlpha=1
+            ctx_.font='45px serif'
+            ctx_.fillStyle='white'
+            for(let i in this.textArray){
+                ctx_.fillText(this.textArray[i],100,100+50*i)
+            }
+            
+        }
+
+        
+
+        ctx_.globalAlpha=1
+
+
+    }
+}
